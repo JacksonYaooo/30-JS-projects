4f39c944b078b1566fde46859c9e7fabc9e22fbc
huangmengyao <2594838054@qq.com>
2023-05-18 13:06:59 +0800

    finished-project 001

--- 001/script.js
+++ 001/script.js
@@ -1,4 +1,4 @@
-// get element  获取类名为变量名
+// get element（获取dom节点）  获取类名为变量名,这里的变量都指的是input标签
 const form = document.getElementById('form');
 const usrname = document.getElementById('usrname');
 const email = document.getElementById('email');
@@ -8,10 +8,13 @@ const password2 = document.getElementById('password2');
 //show input error message 
 function showError(input, message){
     const formControl = input.parentElement;
-    // 返回input的父元素-formControl，仍然是获取类名为变量名
+    // 返回input的父元素-formControl，仍然是获取类名为变量名，这个变量是div标签
     formControl.className = 'form-control error';
+    // 给div加类名，赋相应类名的样式
     const small = formControl.querySelector('small');
+    // 获取文档中第一个 <small> 元素
     small.innerText = message;
+    // 改变small的内容为定义的message
 }
 
 //show success
@@ -22,29 +25,35 @@ function showSuccess(input){
 
 //check email is valid
 function checkEmail(input) {
-    // 正则表达式
+    // 邮箱的正则表达式
     const re = /^([A-Za-z0-9_\-\.])+@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
     if(re.test(input.value.trim())){
+        // trim() 函数的功能是去掉首尾空格
         showSuccess(input);
     } else {
         showError(input, "邮箱格式错误");
     }
 }
 
-//checkRequired input
+//checkRequired input  检查是否输入值
 function checkRequired(inputArr){
     inputArr.forEach(function(input){
         if(input.value.trim()===""){
-            showError(input,`${getKeyWords(input)}为必填项`)
+            showError(input,`${getKeyWords(input)}为必填项`); // ===getKeyWords(input)+"为必填项"
+            // ``是ES6模板字符串的写法，在这里用${}省去了+""，两种写法
+            // getKeyWords()是我自己定义的函数 ↓
         }else{
             showSuccess(input);
         }
     });
+    // 遍历数组，为每一个input都执行一遍function
 }
 
-//get key words
+//getkeyoWrds函数
 function getKeyWords(input){
     return input.placeholder.slice(3);
+    // slice用法：两个参数（索引值）slice(开始位置，结束位置但不包括)，这里是从第四个字（索引为3）开始到最后
+    // 获取的是input标签的placeholder属性里的除“请输入”以外的值
 }
 
 //check length
@@ -58,18 +67,19 @@ function checkLength(input, min, max) {
     }
 }
 
-//check Password Match
+//check Password Match 检查密码匹配
 function checkPasswordsMatch(input1, input2) {
     if(input1.value !== input2.value){
         showError(input2,"密码不匹配");
     }
 }
 
-//event listener
+//event listener（事件监听）绑定事件
 form.addEventListener('submit',function(e) {
-    e.preventDefault();
+    e.preventDefault();  //阻止默认操作的函数（记得给function加上e），使提交表单后页面不刷新
     // if(usrname.value === "") {
     //     showError(usrname,"用户名为必填项")
+            // 给函数传参（input，message）
     // }else{
     //     showSuccess(usrname);
     // }
@@ -90,10 +100,11 @@ form.addEventListener('submit',function(e) {
     // }else{
     //     showSuccess(password2);
     // }
-    // 麻烦方法 ↑
+    // 麻烦方法 ↑  优化方法 ↓
     checkRequired([usrname,email,password,password2]);
+
     checkLength(usrname, 3, 15);
     checkLength(password, 6, 12);
     checkEmail(email);
-    checkPasswordsMatch(password, password2)
+    checkPasswordsMatch(password, password2);
 });
