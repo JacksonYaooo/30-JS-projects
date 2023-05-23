56205f73850ff243cb37251acc2a1ea5d55b79f3
huangmengyao <2594838054@qq.com>
2023-05-23 23:16:48 +0800

    finish

--- 002/script.js
+++ 002/script.js
@@ -3,21 +3,31 @@ const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
-let ticketPrice = +movieSelect.value;
 
 populateUI()
+
+let ticketPrice = +movieSelect.value;
+
+console.log(ticketPrice);
+// value值是string类型，+使其变成可用于计算的num类型
+
 // 更新座位数及总票价
 function updateSelectedCount(){
     const selectedSeats = document.querySelectorAll(".row .seat.selected");
-    // ES6展开运算符
+
+    // ES6展开运算符 (?) ————
+    // [...selectedSeats]是使用展开运算符将selectedSeats数组中的元素复制到一个新数组中。
+    // 这样做是为了确保对原始数组的操作不会影响到原始数据
     const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+    // indexOf()方法返回值在字符串中第一次出现的位置，map遍历数组
+    // 将selectedSeats在数组seats中出现的索引值存进seatsIndex
 
     localStorage.setItem('selectedSeats',JSON.stringify(seatsIndex));
+    // JSON.stringify()将一个JavaScript对象或值转换为 JSON 字符串,localStorage.setItem(key,value)：将value存储到key字段
 
-    const selectedSeatsCount = selectedSeats.length;
+    const selectedSeatsCount = selectedSeats.length;  // 选中的座位数
     count.innerText = selectedSeatsCount;
-    const ticketPrice2 = localStorage.getItem('selectedMoviePrice');
-    total.innerText = selectedSeatsCount * ticketPrice2;
+    total.innerText = selectedSeatsCount * ticketPrice;
 }
 
 // 保存电影索引值与票价
@@ -29,12 +39,14 @@ function setMovieData(movieIndex,moviePrice){
 // 获取本地数据并渲染样式
 function populateUI(){
     const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    // JSON.parse() 方法将数据转换为 JavaScript 对象，相对于JSON.stringify()
     if(selectedSeats !== null && selectedSeats.length > 0){
         seats.forEach((seat, index) =>{
             if(selectedSeats.indexOf(index) > -1){
                 seat.classList.add('selected');
+                // 添加新的类名，如已经存在，取消添加。这里添加类名以获取相应的样式
             }
-        })
+        })  // 渲染样式
     }
 
     const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
@@ -45,6 +57,7 @@ function populateUI(){
 
 // 电影下拉框事件监听
 movieSelect.addEventListener('change', e => {
+    // addEventListener(‘a’,b)如果发生事件a，就执行b函数
     ticketPrice = +e.target.value;
     setMovieData(e.target.selectedIndex,e.target.value);
     updateSelectedCount();
