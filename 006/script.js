9e38571fb9e0a0fd38b49903d07eeea9704cbd34
huangmengyao <2594838054@qq.com>
2023-06-04 18:06:49 +0800

    005-finish

---
+++ 006/script.js
@@ -0,0 +1,16 @@
+const toggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+
+toggle.addEventListener('click', () => document.body.classList.toggle("show-nav"));
+// 点击添加类名，再次点击移除类名
+
+open.addEventListener('click',() => modal.classList.add("show-modal"));
+
+close.addEventListener('click',() => modal.classList.remove("show-modal"));
+// 移除类名
+
+window.addEventListener('click', e => 
+    e.target == modal ? modal.classList.remove("show-modal") : false);
+    // 三元表达式：相当于if else，  格式：条件句 ？ 成立执行语句 ：不成立执行语句 
